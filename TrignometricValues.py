1c199606db5978c94917a80af9debb57c5fcae34
Devika2012-svg <preetraju@gmail.com>
2026-06-27 12:01:05 +0400

    TrignometricValues

---
+++ TrignometricValues.py
@@ -0,0 +1,7 @@
+import math
+
+angle = int(input("Enter the value of the angle: "))
+
+print(f"\n The sin value of {angle} is", math.sin(math.radians(angle)))
+print(f"\n The cos value of {angle} is",math.cos(math.radians(angle)))
+print(f"\n The tangent value of {angle} is", math.tan(math.radians(angle)))
